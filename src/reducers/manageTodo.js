b50272d8a424697bf4bb9c3d099bd4b00ad821ca
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-02-10 14:21:51 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -4,8 +4,7 @@ export default function manageTodo(state = {
   console.log(action)
   switch (action.type) {
     case 'ADD_TODO':
-
-      return { todos: state.todos.concat(action.payload.text) };
+       return { todos: state.todos.concat(action.payload.text) };
 
     default:
       return state;
