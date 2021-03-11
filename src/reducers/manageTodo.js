98371386fc1a2a7b486395499b57b00ddcbfb893
tpham13 <tpham0123@gmail.com>
2021-03-11 13:51:18 -0800

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -10,7 +10,7 @@ export default function manageTodo(state = {
         id: uuid(),
         text: action.payload.text
       }
-      return { todos: state.todos.concat(action.payload.text) };
+      return { todos: state.todos.concat(todo) };
 
     case 'DELETE_TODO':
       return {todos: state.todos.filter(todo => todo.id !== action.payload)}    
